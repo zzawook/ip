14fe527c8413b2e83cba057a196142230bc95d28
zzawook <kjaehyeok21@gmail.com>
2024-08-22 15:29:54 +0800

    Added echo feature to echo the user input
    Created class declaration Dongji and moved all chatbot functionalities to object level declaration

--- src/main/java/Dongji.java
+++ src/main/java/Dongji.java
@@ -1,14 +1,50 @@
+import java.util.Scanner;
+
 public class Dongji {
-    public static void main(String[] args) {
-        greet();
-        exit();
+    private final String SEPARATOR = "-------------------------------";
+
+    private Scanner scanner;
+
+    public Dongji() {
+        this.greet();
+
+        this.scanner = new Scanner(System.in);
+        executeApplication();
+        scanner.close();
+
+        this.exit();
     }
 
-    private static void greet() {
+    private void executeApplication() {
+        String input = this.scanner.nextLine();
+        while (!input.equals("bye")) {
+            this.echo(input);
+            input = scanner.nextLine();
+        }
+    }
+
+    private void echo(String input) {
+        System.out.println(this.wrapWithSeparator(input));
+    }
+
+    private String wrapWithSeparator(String input) {
+        return SEPARATOR + "\n=> " + input + "\n" + SEPARATOR;
+    }
+
+    private void greet() {
+        System.out.println(this.SEPARATOR);
         System.out.println("Hello! I'm Dongji\nWhat can I do for you?");
+        System.out.println(this.SEPARATOR);
     }
 
-    private static void exit() {
+    private void exit() {
+        System.out.println(this.SEPARATOR);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(this.SEPARATOR);
     }
+
+    public static void main(String[] args) {
+        Dongji dongji = new Dongji();
+    }
+
 }
