6cd9572825bf8c61f5b37181ca709ce802d6e4f7
zzawook <kjaehyeok21@gmail.com>
2024-08-22 15:14:33 +0800

    Renamed the project to "Dongji", which means a friend in Korean, and changed all reference to Duke correspondingly
    Added exit message, and separated greeting and exit message to separate function

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Dongji User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Dongji.java
@@ -0,0 +1,14 @@
+public class Dongji {
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I'm Dongji\nWhat can I do for you?");
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
